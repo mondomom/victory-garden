d65d12e71af7d5e1e7b935ccc80bca4113bc1179
mondomom <35747410+mondomom@users.noreply.github.com>
2025-04-30 19:49:43 -0700

    Create watering.py
    initial algorithm for watering.py

---
+++ watering.py
@@ -0,0 +1,13 @@
+# Watering.py
+# Set up i2c 0 (pins 1 & 2)
+# Set up pin 4 LEDOUT
+# Timer function
+  wait about one hour
+# Pump on function
+  count down about 5 minutes
+# Initialize
+# Read the sensor SENSORNOW
+# if sensor reading SENSORNOW < SOILGOOD, run pump_on()
+# pause after MIN minutes 
+# wait ONEHOUR
+# 
